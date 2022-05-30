e4c84ca05877fab852c75abff7d567abc41e2ad7
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-05-30 14:14:56 +0900

    build: add&apply css-modules&react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// local imports
 import logo from './logo.svg'
-import './App.css'
+import styles from './App.module.css'
 
 function App() {
+  const templateHome = (
+    <header className={styles['App-header']}>
+      <img src={logo} className={styles['App-logo']} alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        className={styles['App-link']}
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={templateHome} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
