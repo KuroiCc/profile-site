059564e046a8fbb31f7ecf990f7b98875946adbc
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-05-30 14:48:24 +0900

    feat: create test page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 // local imports
 import logo from './logo.svg'
 import styles from './App.module.css'
+import { TestPage } from './pages'
 
 function App() {
   const templateHome = (
     <header className={styles['App-header']}>
       <img src={logo} className={styles['App-logo']} alt="logo" />
       <p>
-        Edit <code>src/App.tsx</code> and save to reload.
+        Here is the home page <br />
+        Click following link to other page
       </p>
-      <a
-        className={styles['App-link']}
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
+      <Link className={styles['App-link']} to="/testPage">
+        Test Page
+      </Link>
     </header>
   )
   return (
@@ -27,6 +24,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={templateHome} />
+          <Route path="/testPage" element={<TestPage />} />
+          <Route path="*" element={<h1>404 NOT FOUND</h1>} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/testPage/testPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// local imports
+import styles from './testPage.module.css'
+
+export const TestPage: React.FC = () => {
+  return <div className={styles.testPageStage}>testPage</div>
+}
