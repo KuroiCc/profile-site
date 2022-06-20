7cf15281c6c4a43085758fc560c5ad121248bba7
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-06-21 01:58:02 +0900

    feat: Home Page FC

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,32 @@
 import React from 'react'
-import { /* BrowserRouter, */ HashRouter, Routes, Route, Link } from 'react-router-dom'
+import { /* BrowserRouter, */ HashRouter, Routes, Route /* Link */ } from 'react-router-dom'
 
 // local imports
 import './App.css'
-import logo from './logo.svg'
-import { TestPage } from './pages'
+// import logo from './logo.svg'
+import { Home, TestPage } from './pages'
 
 function App() {
-  const templateHome = (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Here is the home page <br />
-          Click following link to other page
-        </p>
-        <Link className="App-link" to="/testPage">
-          Test Page
-        </Link>
-      </header>
-    </div>
-  )
+  // const templateHome = (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Here is the home page <br />
+  //         Click following link to other page
+  //       </p>
+  //       <Link className="App-link" to="/testPage">
+  //         Test Page
+  //       </Link>
+  //     </header>
+  //   </div>
+  // )
   return (
     <>
       {/* <BrowserRouter> */}
       <HashRouter>
         <Routes>
-          <Route path="/" element={templateHome} />
+          <Route path="/" element={<Home />} />
           <Route path="/testPage" element={<TestPage />} />
           <Route path="*" element={<h1>404 NOT FOUND</h1>} />
         </Routes>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// local imports
+import { CodeAnimation } from '../components'
+
+export const Home = () => {
+  return (
+    <div className="bg-zinc-800">
+      <div className="sm:w-5/6 m-auto max-w-7xl sm:py-7">
+        <CodeAnimation />
+      </div>
+    </div>
+  )
+}
