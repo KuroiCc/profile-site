9353aaff5d9717feeef682cfff176ee866e2066d
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-06-20 16:07:03 +0900

    feat: testpage style

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
 export const TestPage: React.FC = () => {
-  return <div>testPage</div>
+  return (
+    <div className="bg-teal-400 m-20 border-8 border-red-500">
+      <p className="font-code">Cascadia Code 400</p>
+    </div>
+  )
 }
