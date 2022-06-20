1771ca0b8f50300da137dace1aa2b3c0aee71f6c
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-06-20 16:06:48 +0900

    feat: extend fontFamily Cascadia Code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        code: [
+          'Cascadia Code',
+          'Menlo',
+          'Monaco',
+          'Consolas',
+          'Courier New',
+          'Roboto Mono',
+          'monospace'
+        ]
+      }
+    }
   },
   plugins: []
 }
