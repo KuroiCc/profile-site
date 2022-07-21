39f84a54029f5d12da4557c40d51c469b048bd51
Kuroi_Cc <chenchengmikuro@gmail.com>
2022-07-22 06:05:41 +0900

    feat: refector date fetch, update styles

--- src/components/ProfileDetail.tsx
+++ src/components/ProfileDetail.tsx
@@ -1,32 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-type Props = {
-  src: string
-  data: { [key: string]: React.ReactNode }[]
-}
+// local imports
+import { TechGallery } from '.'
+import aboutMe from '../assets/aboutMe.json'
+import aboutMeOther from '../assets/aboutMeOther.json'
+import SelfIcon from '../assets/Images/SelfIcon.webp'
 
-export const ProfileDetail: React.FC<Props> = ({ src, data }) => {
+export const ProfileDetail: React.FC = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [data, setData] = useState<Array<{ [key: string]: React.ReactNode }>>([])
+
+  useEffect(() => {
+    setData([
+      ...aboutMe,
+      {
+        tech: (
+          <div>
+            {Object.entries(aboutMeOther.tech).map(([key, value]) => (
+              <TechGallery key={key} title={key} techIcons={value} />
+            ))}
+          </div>
+        )
+      },
+      {
+        'contact me': (
+          <div className="flex flex-col">
+            {aboutMeOther.emails.map((item) => (
+              <a className="hover:underline" href={`mailto:${item}`} key={item.email}>
+                {item.email} {item.memo && <span className="text-sm">({item.memo})</span>}
+              </a>
+            ))}
+          </div>
+        )
+      }
+    ])
+  }, [])
+
   return (
     <section className="text-zinc-100">
       <div className="flex mt-14">
         <h1 className="px-5 text-3xl w-fit bg-rose-600">More about Me</h1>
         <div className="border-[18px] border-t-transparent border-r-transparent border-rose-600 w-0"></div>
       </div>
-      <div className="py-16 px-6">
+      <div className="px-6 py-16">
         <table className="m-auto">
           <tbody>
             <tr className="border-b border-dashed">
-              <td className="pb-4 pl-5" colSpan={2}>
-                <img className="w-20 h-20 rounded-full" src={src} alt="SelfIcon" />
+              <td className="pb-4 pl-6" colSpan={2}>
+                <img className="w-20 h-20 rounded-full" src={SelfIcon} alt="SelfIcon" />
               </td>
             </tr>
             {data.map((item) => {
-              const key = Object.keys(item)[0]
-              const value = Object.values(item)[0] as React.ReactNode
+              const [key, value] = Object.entries(item)[0]
               return (
                 <tr className="border-b border-dashed" key={key}>
-                  <td className="py-3 text-right align-top">{key}:</td>
-                  <td className="py-3 pl-10">{value}</td>
+                  <td className="p-3 text-right capitalize align-top">{key}:</td>
+                  <td className="p-3 pl-10">{value}</td>
                 </tr>
               )
             })}
